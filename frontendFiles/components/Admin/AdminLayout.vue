c075c0fe5feeb7f980a93b7ca2d6e3ea0402d68f
ruste <107406587+Rustem200@users.noreply.github.com>
2025-03-04 12:44:00 +0300

    fourth-commit
    add admin

---
+++ frontendFiles/components/Admin/AdminLayout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex">
+        <aside class="w-64 bg-gray-800 text-white p-4">
+            <h2 class="text-lg font-bold mb-4">Админка</h2>
+            <nav>
+                <router-link to="/admin/users" class="block py-2">Пользователи</router-link>
+                <router-link to="/admin/messages" class="block py-2">Сообщения</router-link>
+                <router-link to="/admin/places" class="block py-2">Места</router-link>
+                <router-link to="/logout" class="block py-2">Выход</router-link>
+            </nav>
+        </aside>
+        <main class="flex-1 p-4">
+            <router-view></router-view>
+        </main>
+    </div>
+</template>
+
+<script>
+export default { name: "AdminLayout" };
+</script>
---
+++ frontendFiles/components/Admin/Index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="mt-4 mb-4">
+            <router-link
+                :to="{ name: 'admin.user.index' }"
+                class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+            >
+                Пользователи
+            </router-link>
+            <router-link
+                :to="{ name: 'admin.message.index' }"
+                class="ml-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700"
+            >
+                Сообщения
+            </router-link>
+            <router-link
+                :to="{ name: 'admin.place.index' }"
+                class="ml-4 px-4 py-2 bg-yellow-500 text-white rounded hover:bg-green-700"
+            >
+                Места
+            </router-link>
+        </div>
+
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AdminIndex",
+};
+</script>
